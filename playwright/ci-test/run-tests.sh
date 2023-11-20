a3f8e73a4a759aaddb906f81c1b16801db117b5f
Jeff Osundwa <jeff@kartoza.com>
2023-11-21 00:20:31 +0300

    Initial set up

---
+++ playwright/ci-test/run-tests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source playwright-path.sh
+
+echo "This script will run the tests defined in tests/"
+echo "Before running the tests you need to create the auth config using this command:"
+echo ""
+echo "./create-auth.sh"
+
+$PLAYWRIGHT \
+    test \
+    --project chromium
+
+echo "--done--"
